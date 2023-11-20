49bc22cb157f28afc46bfaf50afb9e59c3707d77
Rohit <rsgade50@yahoo.com>
2023-11-20 22:18:53 +0530

    first commit

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
+
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
@@ -13,12 +14,7 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-
         return requirements
-
-
-
-
 
 
 setup(
